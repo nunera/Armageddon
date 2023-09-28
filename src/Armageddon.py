ff6ad5d319a40ec06ff5167c60d97c0b75c8f968
nunera <67439647+nunera@users.noreply.github.com>
2023-09-28 21:02:42 +0000

    please help me im going to kill myself

---
+++ src/Armageddon.py
@@ -0,0 +1,13 @@
+"""Main file for Armageddon."""
+import timeit
+from selenium import webdriver
+
+path = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+
+timeit.default_timer()
+
+Options = webdriver.ChromeOptions
+Options.add_argument(Options,"--headless")
+Options.add_argument(Options,"--window-size=1920x1080")
+
+webdriver.Chrome(Options)
